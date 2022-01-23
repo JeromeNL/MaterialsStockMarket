33a013e671e8e84a5d13811ebd672870cc7d2955
JeromeNL <jw.kwetters@student.avans.nl>
2022-01-23 18:57:03 +0100

    Small error message fix

--- src/main/java/me/joram/materialsstockmarket/PlayerCommands.java
+++ src/main/java/me/joram/materialsstockmarket/PlayerCommands.java
@@ -39,20 +39,14 @@ public class PlayerCommands implements CommandExecutor {
                     Inventory overViewInv = pGUI.generateOverViewGUI();
                     player.openInventory(overViewInv);
 
-                }
-
-
-
-
-                else if (args[0].equalsIgnoreCase("adddays")) {
+                } else if (args[0].equalsIgnoreCase("adddays")) {
                     if (!(player.hasPermission("MaterialsStockMarket.adddays"))) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " You do not have permission to use this command!");
                         return false;
                     }
-                    if(!(args.length == 2)){
+                    if (!(args.length == 2)) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use: /mm adddays <amountOfDays>");
-                    }
-                    else{
+                    } else {
                         try {
                             int amount = Integer.valueOf(args[1]);
 
@@ -61,15 +55,11 @@ public class PlayerCommands implements CommandExecutor {
                                 auto.runAutoPricing();
                             }
                             player.sendMessage(ChatColor.DARK_GREEN + "Added " + ChatColor.GREEN + amount + ChatColor.DARK_GREEN + " days to the stockmarket!");
-                        } catch(Exception e){
+                        } catch (Exception e) {
                             player.sendMessage(ChatColor.DARK_RED + "ERROR!");
                         }
                     }
-                }
-
-
-
-                    else if (args[0].equalsIgnoreCase("depression")) {
+                } else if (args[0].equalsIgnoreCase("depression")) {
                     if (!(player.hasPermission("MaterialsStockMarket.depression"))) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " You do not have permission to use this command!");
                         return false;
@@ -79,25 +69,20 @@ public class PlayerCommands implements CommandExecutor {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use: /mm depression <percentage without decimals/- sign>");
                     } else {
                         try {
-                             percentage = Integer.valueOf(args[1]);
-                            if((percentage > 0 && percentage < 100)) {
+                            percentage = Integer.valueOf(args[1]);
+                            if ((percentage > 0 && percentage < 100)) {
                                 AutoPricing auto = new AutoPricing(main);
                                 auto.changeAllPrices(percentage * -1);
-                                player.sendMessage(ChatColor.DARK_RED + "Depression added of: -" + ChatColor.RED +  percentage + ChatColor.DARK_RED + "%");
+                                player.sendMessage(ChatColor.DARK_RED + "Depression added of: -" + ChatColor.RED + percentage + ChatColor.DARK_RED + "%");
                                 Bukkit.broadcastMessage("Due to poor economic conditions, the stock market has fallen by " + percentage + "%");
-                            }
-                            else{
+                            } else {
                                 player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use a value between 1 and 99 percent!");
                             }
-                        }catch(Exception e) {
+                        } catch (Exception e) {
                             player.sendMessage(ChatColor.RED + "Use a value between 1 and 99!");
                         }
                     }
-                }
-
-
-
-                 else if(args[0].equalsIgnoreCase("help")) {
+                } else if (args[0].equalsIgnoreCase("help")) {
 
                     if (!(player.hasPermission("MaterialsStockMarket.help"))) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " You do not have permission to use this command!");
@@ -112,48 +97,75 @@ public class PlayerCommands implements CommandExecutor {
                     player.sendMessage(ChatColor.LIGHT_PURPLE + "Use /mm changesellprice <item> <amount> to change the sell price of an item.");
                     player.sendMessage(ChatColor.DARK_PURPLE + "--------------==()==--------------");
                 }
-                 // /changebuyprice ITEM NEWPRICE
+                // /changebuyprice ITEM NEWPRICE
 
-                 else if(args[0].equalsIgnoreCase("changebuyprice")){
+                else if (args[0].equalsIgnoreCase("changebuyprice")) {
                     if (!(player.hasPermission("MaterialsStockMarket.changebuyprice"))) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " You do not have permission to use this command!");
                         return false;
                     }
 
-                    if(!(dataController.itemExists(args[1]))){
+                    if (args.length != 3) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " Use: /mm changebuyprice <item> <new price>");
+                        return false;
+                    }
+
+                    if (!(dataController.itemExists(args[1]))) {
                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " This item is not part of the stock market!");
                         return false;
                     }
 
-                     Double amount = 0.0;
-                     if(args.length != 3){
-                         return false;
-                     }
-                         try{
-                             amount = Double.parseDouble(args[2]);
-                         }catch(Exception e){
-                             player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "This is not a valid amount!");
-                         }
+                    Double amount = 0.0;
+                    try {
+                        amount = Double.parseDouble(args[2]);
+                    } catch (Exception e) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "This is not a valid amount!");
+                    }
 
-                     if(!(amount > 0 && amount < 100000 )){
-                         player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use a amount between 1 and 100.000");
+                    if (!(amount > 0 && amount < 100000)) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use a amount between 1 and 100.000");
                         return false;
-                     }
-                        dataController.addToPrices(Material.valueOf(args[1].toUpperCase()), amount, dataController.getSellPrice(Material.valueOf(args[1]), 1));
-                        player.sendMessage(ChatColor.GREEN + "You have changed the price of " + args[1] + " to " + amount);
-                }
+                    }
+                    dataController.addToPrices(Material.valueOf(args[1].toUpperCase()), amount, dataController.getSellPrice(Material.valueOf(args[1].toUpperCase()), 1));
+                    player.sendMessage(ChatColor.GREEN + "You have changed the buy price of " + args[1] + " to " + amount);
+                } else if (args[0].equalsIgnoreCase("changesellprice")) {
+                    if (!(player.hasPermission("MaterialsStockMarket.changesellprice"))) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " You do not have permission to use this command!");
+                        return false;
+                    }
 
-                else if(args[0].equalsIgnoreCase("changesellprice")){
+                    if (args.length != 3) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " Use: /mm changesellprice <item> <new price>");
+                        return false;
+                    }
 
-                }
+                    if (!(dataController.itemExists(args[1]))) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " This item is not part of the stock market!");
+                        return false;
+                    }
 
-                else{
+                    Double amount = 0.0;
+                    try {
+                        amount = Double.parseDouble(args[2]);
+                    } catch (Exception e) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "This is not a valid amount!");
+                    }
+
+                    if (!(amount > 0 && amount < 100000)) {
+                        player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + "Use a amount between 1 and 100.000");
+                        return false;
+                    }
+                    dataController.addToPrices(Material.valueOf(args[1].toUpperCase()), dataController.getBuyPrice(Material.valueOf(args[1].toUpperCase()), 1), amount);
+                    player.sendMessage(ChatColor.GREEN + "You have changed the sell price of " + args[1] + " to " + amount);
+                } else{
                     player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " This command does not exist!");
                 }
-            }
-            else{
+
+            } else {
                 player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " This command does not exist!");
             }
+        } else {
+            player.sendMessage(ChatColor.DARK_RED + "Error! " + ChatColor.RED + " This command does not exist!");
         }
         return false;
     }
